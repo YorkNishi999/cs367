454310a0a0ed604895f9d9160f43642dc3a3f45d
YorkNishi999 <york.nishi@gmail.com>
2021-09-04 01:43:02 -0500

    first com

---
+++ src/assign1/EmployeeDatabase.java
@@ -0,0 +1,235 @@
+package assign1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+//interface EmployeeData {
+//	public abstract void addEmployee(String e);
+//}
+
+//public class EmployeeDatabase implements EmployeeData{
+public class EmployeeDatabase {
+	// field variables
+	// Employeeクラスを要素として持つArrayList
+	private static ArrayList<Employee> edb = new ArrayList<>();
+	
+	// Constructor
+	public EmployeeDatabase() {	
+	}
+
+	// methods
+	public void addEmployee(String e) {
+		// 与えられたユーザ名 e を持つ従業員をデータベースの最後に追加します。
+		// もし、ユーザ名eの従業員がすでにデータベースに存在する場合は、
+		// 単にリターンします。
+		if (e == null) {
+			throw new java.lang.IllegalArgumentException();
+		}
+		Employee em = new Employee(e);
+		// 1. 最初から最後まで、Eの従業員を検索する
+		// 2. あれば→そこでBreak
+		// 3. なければ→最後にUserを追加する
+		for ( int i = 0; i < edb.size(); ++i ) {
+			if ( edb.get(i).getUsername() == e ) {
+				return;
+			}
+		}
+		edb.add(edb.size(), em);
+		return;
+	} // end of addEmployee
+	
+	public void addDestination(String e, String d) {
+		// データベースに登録されている従業員eのウィッシュリストに、
+		// 与えられた目的地dを追加します。従業員eがデータベースにない場合は、
+		// java.lang.IllegalArgumentExceptionをスローします。
+		// 従業員eのウィッシュリストにdが既に登録されている場合は、そのまま戻ります。
+		// local variables
+		
+		if (e == null || d == null) {
+			throw new java.lang.IllegalArgumentException();
+		}
+		
+		List<String> wi = new ArrayList<String>();
+		
+		// 1. DBからEmployeeの名前を取り出す
+		// 2. それがeであるかをチェック
+		// 3. Yes＞目的地を取り出す　No＞次の人へ。なければErrorをスロー
+		// 4. それがdであるかをチェック
+		// 5. Yes＞何もせず
+		// 6. No＞最後に追加する
+		
+		for ( int i = 0; i < edb.size(); ++i ) {
+			if ( edb.get(i).getUsername() == e ) {
+				wi = edb.get(i).getWishlist();
+				for (int j = 0; j < wi.size(); ++j) {
+					if(wi.get(j) == d) {
+						// Do nothing
+						return;
+					}
+				}
+				wi.add(d);
+				return;
+			}
+		}
+		throw new java.lang.IllegalArgumentException();
+		
+	}// end of addDestination
+		
+	boolean containsEmployee(String e) {
+		// 社員eがデータベースに登録されている場合のみ、trueを返します。
+		if (e == null) {
+			throw new java.lang.IllegalArgumentException();
+		}
+		for ( int i = 0; i < edb.size(); ++i ) {
+			if ( edb.get(i).getUsername() == e ) {
+				return true;
+			}
+		}
+		return false;
+	} // end of containsEmployee
+	
+	boolean containsDestination(String d) {
+		// 目的地dがデータベース内の少なくとも1つの社員のウィッシュリストに
+		// 表示されている場合に限り、trueを返します。
+		if (d == null) {
+			throw new java.lang.IllegalArgumentException();
+		}
+		
+		for ( int i = 0; i < edb.size(); ++i ) {
+			for (int j = 0; j < edb.get(i).getWishlist().size(); ++j) {
+				if(edb.get(i).getWishlist().get(j) == d) {
+					return true;
+				}
+			}	
+		}
+		return false;
+	} // end of containsDestination
+		
+	boolean hasDestination(String e, String d) {
+		// 目的地dが従業員eのウィッシュリストに入っている場合のみ、trueを返します。
+		if (e == null || d == null) {
+			throw new java.lang.IllegalArgumentException();
+		}
+		
+		List<String> wi = new ArrayList<String>();
+				
+		for ( int i = 0; i < edb.size(); ++i ) {
+			if ( edb.get(i).getUsername() == e ) {
+				wi = edb.get(i).getWishlist();
+				for (int j = 0; j < wi.size(); ++j) {
+					if(wi.get(j) == d) {
+						return true;
+					}
+				}
+				return false;
+			}
+		}
+		return false;
+	} // end of has Destination
+	
+	List<String> getEmployees(String d) {
+		// 目的地dをウィッシュリストに入れている社員のリストを返します。
+		// 目的地dがデータベースにない場合は、NULLのリストを返します。
+		if (d == null) {
+			throw new java.lang.IllegalArgumentException();
+		}
+		
+		List<String> res = new ArrayList<String>();
+		
+		for ( int i = 0; i < edb.size(); ++i ) {
+			for (int j = 0; j < edb.get(i).getWishlist().size(); ++j) {
+				if(edb.get(i).getWishlist().get(j) == d) {
+					res.add(edb.get(i).getUsername());
+				}
+			}	
+		}
+		return res;
+		
+	} // end of getEmplyyees
+	
+	List<String> getDestinations(String e) {
+		// 従業員eのウィッシュリストを返します。
+		// 従業員eがデータベースに存在しない場合は、nullを返します。
+		if (e == null) {
+			throw new java.lang.IllegalArgumentException();
+		}
+		
+		List<String> res = new ArrayList<String>();
+		
+		for ( int i = 0; i < edb.size(); ++i ) {
+			if ( edb.get(i).getUsername() == e ) {
+				res = edb.get(i).getWishlist();
+			}
+		}
+		if (res != null) {
+			return res;
+		} else {
+			return null;
+		}
+		
+	} // end of getDestinations
+	
+	Iterator<Employee> iterator(){
+		// データベース内の Employee オブジェクトの Iterator を返します。
+		// 従業員は、データベースに追加された順 (テキスト・ファイルに含まれる順) 
+		// で返される必要があります。
+		
+		Iterator<Employee> ei = edb.iterator();
+		return ei;
+		
+	} // end of iterator
+	
+	boolean removeEmployee(String e) {
+		// データベースから従業員eを削除します。従業員eがデータベースに存
+		// しない場合は、falseを返し、そうでない場合（つまり、削除が成功した
+		// 場合）は、trueを返します。
+		
+		if (e == null) {
+			throw new java.lang.IllegalArgumentException();
+		}
+		
+		while (edb.iterator().hasNext()) {
+			if (edb.iterator().next().getUsername() == e) {
+				edb.iterator().remove();
+				return true;
+			}
+		}
+		return false;
+	} // end of removeEmployee
+
+	boolean removeDestination(String d) {
+		// データベースから目的地dを削除する、つまり、目的地dが登場するすべての
+		// ウィッシュリストから目的地dを削除する。目的地dがデータベースに存在しな
+		// い場合はfalseを、そうでない場合（つまり削除が成功した場合）はtrueを返
+		// します。
+		// List<String> res = new ArrayList<String>();
+		
+		if (d == null) {
+			throw new java.lang.IllegalArgumentException();
+		}
+		
+		for ( int i = 0; i < edb.size(); ++i ) {
+			for (int j = 0; j < edb.get(i).getWishlist().size(); ++j) {
+				if(edb.get(i).getWishlist().get(j) == d) {
+					return edb.get(i).getWishlist().remove(d);
+				}
+			}	
+		}
+		return false;
+		
+	} // end of removeDestination
+	
+	int size() {
+		int counter = 0;
+		
+		while (edb.iterator().hasNext()) {
+			++counter;
+		}
+		return counter;
+	} // end of size
+	
+	
+
+} // end of class
---
+++ src/assign1/InteractiveDBTester.java
@@ -0,0 +1,146 @@
+package assign1;
+
+import java.util.*;
+import java.io.*;
+//import java.lang.Exception;
+
+public class InteractiveDBTester {
+	
+	// make the Employee database a static data member so it does not have to 
+	// be passed to each help method
+        // It is protected so that subclasses representing particular testers can
+        // access it
+
+	protected static EmployeeDatabase EmployeeDB = new EmployeeDatabase();
+	
+        // Initialize DB from external data file
+        protected static void populateDB(String [] args) {
+    	
+    	// Step 1: check whether exactly one command-line argument is given
+	   /* Code to implement Step 1 goes here  */
+//        	if(args[1] != null) {
+//        		throw new java.lang.IllegalArgumentException();
+//        	}
+        		
+    	
+    	// Step 2: check whether the input file exists and is readable
+	   /* Code to implement Step 2 goes here  */
+    	
+    	// Step 3: load the data from the input file and use it to construct a
+    	//         Employee database
+     	   /* Code to implement Step 3 goes here  */
+        	FileReader fr = null;
+        	BufferedReader br = null;
+        	
+        	String tmp;
+        	String[] em;
+        	try {
+        		fr = new FileReader("/home/yohei-pana/project/eclipse-workspace/CS367/assgin1/assign1/emp.txt");
+        		br = new BufferedReader(fr);
+        		
+        		while((tmp = br.readLine() ) != null) {
+        			System.out.println(tmp);
+        			em = tmp.split(",");
+        			System.out.println(em[0]);
+        			EmployeeDB.addEmployee(em[0]);
+        			for (int i = 1; i < em.length; ++i) {
+        				EmployeeDB.addDestination(em[0], em[i]);
+        			}	
+        		}
+        	} catch (IOException e) {
+        		System.out.println(e);
+        	}
+        	
+        	System.out.println("db size is ");
+        	System.out.println(EmployeeDB.size());
+        	System.out.println(".");
+       } // end of populateDB
+
+     static boolean GUIactive;  //is GUI tester active?
+
+// Methods that implement GUI buttons or testing actions
+
+    protected static String pushFind(String employee){
+       /* Code to implement find command goes here:
+          Find the supplied employee in the employee database
+       */
+    	String msg;
+    	if(EmployeeDB.containsEmployee(employee)) {
+    		System.out.println("find!");
+    		msg = "find"; 
+    		return msg;
+    	}
+    	System.out.println("no found!");
+    	return "Notiong find";
+    }
+
+    protected static String pushDiscontinue(String destination){
+       /* Code to implement discontinue command goes here:
+          The supplied destination is removed from the wish lists
+           of all employees in the employee database
+       */
+    }
+
+    protected static String pushSearch(String destination){
+       /* Code to implement search command goes here:
+           Search the employee database for all employees who have
+            the supplied destination in their wish list
+       */
+    }
+
+    protected static String pushRemove(String employee){
+       /* Code to implement remone command goes here:
+          Remove the supplied employee from the employee database
+       */
+    }
+
+    protected static String pushInformation(){
+       /* Code to implement information command goes here:
+           Compute key information on the state of the employee database
+       */
+    }
+
+    protected static String pushList(){
+       /* Code to implement list command goes here:
+          List the current contents of the employee database
+       */
+    }
+
+    // The pushHelp method may be used as is:
+
+    protected static String pushHelp(){
+        String cmds = "";
+	if (GUIactive) {
+          cmds +="Available commands:\n" +
+                "\tFind employee\n" +
+                "\tDiscontinue destination\n" +
+                "\tSearch destination\n" +
+                "\tRemove employee\n" +
+                "\tInformation on database\n" +
+                "\tList contents of database\n" +
+                "\tText interface activated\n" +
+                "\tHelp on available commands\n" +
+                "\tQuit database testing\n" ;
+       }else {
+          cmds +=
+        	("discontinue/d <destination> - discontinue the given <destination>\n") +
+        	("find/f <Employee> - find the given <Employee>\n") +
+        	("gui/g Switch to GUI testing interface\n") +
+        	("help/h - display this help menu\n") +
+        	("information/i - display information about this Employee database\n") +
+        	("list/l - list contents of Employee database\n") +
+        	("search/s <destination> - search for the given <destination>\n") +
+        	("quit/q - quit\n") +
+        	("remove/r <Employee> - remove the given <Employee>\n");
+         
+       }
+            return cmds;
+    }
+
+    // The pushQuit method may be used as is:
+
+    protected static String pushQuit(){
+        System.exit(0);
+        return "";
+    }
+}
